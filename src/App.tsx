84e9e20c90ba9f796ffe7ee26ff5dca3e2bec8e1
Lim Dong Hyeon <ilhai6@naver.com>
2024-10-23 13:36:53 +0900

    Feat: delete 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Form from "./components/Form";
 
 interface FormDataType {
+  id: number;
   name: string;
   text: string;
 }
@@ -10,31 +11,33 @@ interface FormDataType {
 function App() {
   const [formData, setFormData] = useState<FormDataType[]>([]);
   const handleSubmit = (name: string, text: string) => {
+    if (!name || !text) return alert("빈칸을 채워주세요.");
     setFormData((prev) => [
       ...prev,
       {
+        id: Date.now(),
         name,
         text,
       },
     ]);
   };
-  const handleDelete = () => {
-    console.log(Date.now());
+  const handleDelete = (id: number) => {
+    setFormData((prev) => prev.filter((item) => item.id !== id));
   };
   return (
     <main>
       <h1>React만으로 TodoList 개발</h1>
       <Form handleSubmit={handleSubmit} />
       <ul>
-        {formData.map(({ name, text }, index) => {
+        {formData.map(({ name, text, id }) => {
           return (
-            <li key={index}>
+            <li key={id}>
               <div>{name}</div>
               <div>{text}</div>
               <ul>
                 <li>
                   <button type="button">수정</button>
-                  <button type="button" onClick={handleDelete}>
+                  <button type="button" onClick={() => handleDelete(id)}>
                     삭제
                   </button>
                 </li>
